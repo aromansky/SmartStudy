f56627d73b2ffa4beaebf21cf363257d7aebf8a9
aromansky <kirbi.zsthan@gmail.com>
2023-10-18 17:28:23 +0300

    Сделал прототип UI стартовой страницы

---
+++ SmartStudy/FirstPage.xaml.cs
@@ -0,0 +1,28 @@
+namespace SmartStudy;
+
+public partial class FirstPage : ContentPage
+{
+	public FirstPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ClickRegister(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ClickLogIn(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ClickForgotPassword(object sender, EventArgs e)
+    {
+
+    }
+
+    private void TextChanged(object sender, TextChangedEventArgs e)
+    {
+    }
+}
