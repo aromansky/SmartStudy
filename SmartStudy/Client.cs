7ec5f6a6f6d49cca6fa0cf99c390e75976764300
aromansky <kirbi.zsthan@gmail.com>
2023-11-07 00:05:14 +0300

    Работа с БД
    -Создал класс для работы с БД
    -Создал config-файл

---
+++ SmartStudy/Client.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+
+
+namespace SmartStudy
+{
+    static class Client
+    {
+        static MySqlConnection connect = new MySqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+
+        public static void Register(string FirstName, string LastName, string Email, string Password)
+        {
+            connect.Open();
+
+            MySqlCommand command = new MySqlCommand(
+                "INSERT INTO user (FirstName, LastName, Email, Password)" +
+                $"VALUES (@FirstName, @LastName, @Email, @Password)", connect
+                );
+
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@Password", Password);
+
+            command.ExecuteNonQuery();
+
+
+            connect.Close();
+        }
+    }
+}
--- SmartStudy/RegistrationPage.xaml.cs
+++ SmartStudy/RegistrationPage.xaml.cs
@@ -9,7 +9,10 @@ public partial class RegistrationPage : ContentPage
 
     private void CreateAccount(object sender, EventArgs e)
     {
-
+        if(Password.Text == RepeatPassword.Text)
+        {
+            Client.Register(FirstName.Text, LastName.Text, EMail.Text, Password.Text);
+        }
     }
 
     private void ClickPasswordVisibility(object sender, EventArgs e)
