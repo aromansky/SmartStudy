3619776707098dc26b0b8c56081c7787cc2087a6
aromansky <kirbi.zsthan@gmail.com>
2023-11-13 13:23:24 +0300

    Добавил модели и контроллеры
    Добавил модели:
    -event
    -group_event
    Добавил контроллеры:
    -eventController
    -group_eventController

---
+++ WebAPI/Models/event.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class @event
+    {
+        [Key]
+        public long event_id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ WebAPI/Models/group_event.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebAPI.Models
+{
+    public class group_event
+    {
+        [Key]
+        public long group_event_id { get; set; }
+        public long event_id { get; set; }
+        public long group_settings_id { get; set; }
+        public required DateTime date_begin { get; set; }
+        public required DateTime date_end { get; set; }
+    }
+}
