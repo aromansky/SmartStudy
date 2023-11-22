afd0705ebe2200ded4014e551e9850c9aca0c353
Mlynnik Semen <mlynnik@sfedu.ru>
2023-11-22 22:49:53 +0300

    исправил календарь
    В файле Models\Calendar_note.cs подробная документация

--- SmartStudy/Models/Calendar_note.cs
+++ SmartStudy/Models/Calendar_note.cs
@@ -3,56 +3,63 @@ using System.Data;
 
 namespace SmartStudy.Models
 {
+    //класс Note - близнец класса @event, оставил пока что его, т.к. потом надо будет
+    //менять взаимодействие с event_id
+    //у меня они генерируются для конкретного пользователя, а должны глобально где-то в бд
+    //
+    //если я неправильно понял, можно сразу заменить его на @event
+    //с которым по идее должна быть интеграция у бд
+    //
+    //вообще можно создать функции в бд и вызывать их из моих, с уже переданными
+    //параметрами(id, строки, даты), если это удобно 
     internal class Note
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name_note { get; set; }
-        public string Date_note { get; set; }
-        public string Time_note { get; set; }
         public string Text_note { get; set; }
+        public DateTime Date_begin { get; set; }
+        public DateTime Date_end { get; set; }
     }
-    internal class Note_for_sort
-    {
-        public string Name_note { get; set; }
-        public DateTime Date_note { get; set; }
-        public TimeSpan Time_note { get; set; }
-        public string Text_note { get; set; }
-    }
-
     internal class Calendar_note
     {
         private string AppDataPath = FileSystem.AppDataDirectory;
+        //коллекция всех событий пользователя
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
         public void create_data()
         {
             DataSet usersSet = new DataSet("UsersSet");
             DataTable users = new DataTable("Users");
             usersSet.Tables.Add(users);
 
-            DataColumn idColumn = new DataColumn("Id", Type.GetType("System.Int32"));
+            DataColumn idColumn = new DataColumn("Id", Type.GetType("System.Int64"));
             idColumn.Unique = true; // столбец будет иметь уникальное значение
             idColumn.AllowDBNull = false; // не может принимать null
             idColumn.AutoIncrement = true; // будет автоинкрементироваться
             idColumn.AutoIncrementSeed = 0; // начальное значение
             idColumn.AutoIncrementStep = 1; // приращении при добавлении новой строки
-
+            
             DataColumn nameColumn = new DataColumn("Name", Type.GetType("System.String"));
-            DataColumn dateColumn = new DataColumn("Date", Type.GetType("System.String"));
-            DataColumn timeColumn = new DataColumn("Time", Type.GetType("System.String"));
             DataColumn textColumn = new DataColumn("Text", Type.GetType("System.String"));
+            DataColumn date_begin_Column = new DataColumn("Date_begin", Type.GetType("System.DateTime"));
+            DataColumn date_end_Column = new DataColumn("Date_end", Type.GetType("System.DateTime"));
             textColumn.DefaultValue = ""; // значение по умолчанию
 
             users.Columns.Add(idColumn);
             users.Columns.Add(nameColumn);
-            users.Columns.Add(dateColumn);
-            users.Columns.Add(timeColumn);
             users.Columns.Add(textColumn);
+            users.Columns.Add(date_begin_Column);
+            users.Columns.Add(date_end_Column);
 
             users.PrimaryKey = new DataColumn[] { users.Columns["Id"] };
 
             users.WriteXml(AppDataPath+"local_calendar.xml", XmlWriteMode.WriteSchema);
         }
-        public void add_data(string Name_note, string Date_note, string Time_note, string Text_note)
+        //когда появится взаимодействие с бд, дублировать это же для класса @event
+        //в связке с какой-то функцией добавления события в бд
+        //и соответственно после добавления в бд запись в локальный файл моей функцией с переданным event_id
+        public void add_data(string Name_note, string Text_note, DateTime Date_begin, DateTime Date_end)
         {
+            
             DataSet usersSet = new DataSet("UsersSet");
             DataTable users = new DataTable("Users");
             usersSet.Tables.Add(users);
@@ -60,15 +67,18 @@ namespace SmartStudy.Models
                 create_data();
             users.ReadXml(AppDataPath+"local_calendar.xml");
 
-            users.Rows.Add(new object[] { null, Name_note, Date_note, Time_note, Text_note });
+            users.Rows.Add(new object[] { null, Name_note, Text_note, Date_begin, Date_end });
 
             users.WriteXml(AppDataPath+"local_calendar.xml", XmlWriteMode.WriteSchema);
             sort_data();
         }
-        private ObservableCollection<Note_for_sort> Notes_for_sort { get; set; } = new ObservableCollection<Note_for_sort>();
+        
+        //функция существует пока нет добавления события в события в бд
+        //предназначена для сортировки по времени, так что можно
+        //оставить для оффлайн версии, если такая будет, единственно заменять note_id на event_id
         private void sort_data()
         {
-            Notes_for_sort.Clear();
+            Notes.Clear();
 
             DataSet usersSet = new DataSet("UsersSet");
             DataTable users = new DataTable("Users");
@@ -78,28 +88,30 @@ namespace SmartStudy.Models
             users.ReadXml(AppDataPath+"local_calendar.xml");
             foreach (DataRow r in users.Rows)
             {
-                Note_for_sort note = new Note_for_sort();
+                Note note = new Note();
                 note.Name_note = r.ItemArray[1].ToString();
-                note.Date_note = DateTime.ParseExact(r.ItemArray[2].ToString(), "dd.MM.yyyy", null);
-                note.Time_note = TimeSpan.ParseExact(r.ItemArray[3].ToString(), "hh\\:mm", null);
-                note.Text_note = r.ItemArray[4].ToString();
-                Notes_for_sort.Add(note);
+                note.Text_note = r.ItemArray[2].ToString();
+                note.Date_begin = DateTime.ParseExact(r.ItemArray[3].ToString(), "G", null);
+                note.Date_end = DateTime.ParseExact(r.ItemArray[4].ToString(), "G", null);
+                Notes.Add(note);
             }
-            var sortedNotes = Notes_for_sort.OrderBy(p => p.Date_note).ThenBy(p => p.Time_note);
+            var sortedNotes = Notes.OrderBy(p => p.Date_begin).ThenBy(p => p.Date_end);
             users.Rows.Clear();
-            int i = 0;
-            foreach (Note_for_sort note in sortedNotes)
+            long i = 0;
+            foreach (Note note in sortedNotes)
             {
-                users.Rows.Add(new object[] { i, note.Name_note, note.Date_note.ToString("dd.MM.yyyy"), 
-                        note.Time_note.ToString("hh\\:mm"), note.Text_note });
+                users.Rows.Add(new object[] { i, note.Name_note, note.Text_note, 
+                    note.Date_begin, note.Date_end });
                 i ++;
             }    
                 
             users.WriteXml(AppDataPath+"local_calendar.xml", XmlWriteMode.WriteSchema);
         }
-        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
         public Calendar_note() =>
             Load_All_Notes();
+        
+        //загружает все события,
+        //в неё можно поместить функцию из бд для притягивания всех событий пользователя
         public void Load_All_Notes()
         {
             Notes.Clear();
@@ -113,15 +125,18 @@ namespace SmartStudy.Models
             foreach (DataRow r in users.Rows)
             {
                 Note note = new Note();
-                note.Id = (int)r.ItemArray[0];
+                note.Id = (long)r.ItemArray[0];
                 note.Name_note = r.ItemArray[1].ToString();
-                note.Date_note = r.ItemArray[2].ToString();
-                note.Time_note = r.ItemArray[3].ToString();
-                note.Text_note =  r.ItemArray[4].ToString();
+                note.Text_note =  r.ItemArray[2].ToString();
+                note.Date_begin = DateTime.ParseExact(r.ItemArray[3].ToString(), "G", null);
+                note.Date_end = DateTime.ParseExact(r.ItemArray[4].ToString(), "G", null);
                 Notes.Add(note);
             }
         }
-        public void Save_edit_note(int editing_Id, string Name_note, string Date_note, string Time_note, string Text_note)
+
+        //сохраняет изменения в событии с id=editing_Id
+        //аналогично можно воспользоваться передаваемыми параметрами для вызова функции из бд
+        public void Save_edit_note(long editing_Id, string Name_note, string Text_note, DateTime Date_begin, DateTime Date_end)
         {
             DataSet usersSet = new DataSet("UsersSet");
             DataTable users = new DataTable("Users");
@@ -134,14 +149,15 @@ namespace SmartStudy.Models
             foreach (var note_editing in selectedUsers)
             {
                 note_editing["Name"] = Name_note;
-                note_editing["Date"] = Date_note;
-                note_editing["Time"] = Time_note;
                 note_editing["Text"] = Text_note;
+                note_editing["Date_begin"] = Date_begin;
+                note_editing["Date_end"] = Date_end;
             }
             users.WriteXml(AppDataPath+"local_calendar.xml", XmlWriteMode.WriteSchema);
             sort_data();
         }
-        public void Delete_note(int editing_Id)
+        //удаляет событие id=editing_Id
+        public void Delete_note(long editing_Id)
         {
             DataSet usersSet = new DataSet("UsersSet");
             DataTable users = new DataTable("Users");
@@ -154,6 +170,11 @@ namespace SmartStudy.Models
             foreach (var note_editing in selectedUsers)
                 note_editing.Delete();
             users.WriteXml(AppDataPath+"local_calendar.xml", XmlWriteMode.WriteSchema);
+        }
+        //считывает параметры события с id=editing_Id из списка всех событий
+        public Note Get_Note_By_Id(long editing_Id)
+        {
+            return Notes.Where(x => x.Id == editing_Id).FirstOrDefault();
         }
     }
 }
--- SmartStudy/Views/Teacher/Calendar_note_add.xaml.cs
+++ SmartStudy/Views/Teacher/Calendar_note_add.xaml.cs
@@ -9,21 +9,33 @@ public partial class Calendar_note_add : ContentPage
     private async void SaveButton_Clicked(object sender, EventArgs e)
     {
         Models.Calendar_note calendar_Note = new Models.Calendar_note();
-        calendar_Note.add_data(Note_Name_entry.Text, date_change.Date.ToString("dd.MM.yyyy"),
-            time_change.Time.ToString("hh\\:mm"), TextEditor.Text);
+        calendar_Note.add_data(Note_Name_entry.Text, TextEditor.Text, 
+            DateTime.ParseExact(date_change_begin.Date.ToString("dd.MM.yyyy") + " " +
+            time_change_begin.Time.ToString("hh\\:mm"), "g", null),
+            DateTime.ParseExact(date_change_end.Date.ToString("dd.MM.yyyy") + " " +
+            time_change_end.Time.ToString("hh\\:mm"), "g", null));
         await Shell.Current.GoToAsync("///calendar");
     }
     private async void CancelButton_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("///calendar");
     }
-    public void date_selected(object sender, DateChangedEventArgs e)
+    public void date_begin_selected(object sender, DateChangedEventArgs e)
     {
-        all_date.Text = e.NewDate.ToString("dd.MM.yyyy") + " " + time_change.Time.ToString("hh\\:mm");
+        all_date_begin.Text = e.NewDate.ToString("dd.MM.yyyy") + " " + time_change_begin.Time.ToString("hh\\:mm");
     }
-    public void time_selected(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    public void time_begin_selected(object sender, System.ComponentModel.PropertyChangedEventArgs e)
     {
         if (e.PropertyName == "Time")
-            all_date.Text = date_change.Date.ToString("dd.MM.yyyy") + " " + $"{time_change.Time.ToString("hh\\:mm")}";
+            all_date_begin.Text = date_change_begin.Date.ToString("dd.MM.yyyy") + " " + $"{time_change_begin.Time.ToString("hh\\:mm")}";
+    }
+    public void date_end_selected(object sender, DateChangedEventArgs e)
+    {
+        all_date_end.Text = e.NewDate.ToString("dd.MM.yyyy") + " " + time_change_end.Time.ToString("hh\\:mm");
+    }
+    public void time_end_selected(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == "Time")
+            all_date_end.Text = date_change_end.Date.ToString("dd.MM.yyyy") + " " + $"{time_change_end.Time.ToString("hh\\:mm")}";
     }
 }
