36c84cd7c9e3f87c5bd0eda98cf9444827cb908d
HUAWEI <sanchous2810@gmail.com>
2023-11-25 00:39:36 +0300

    Добавил CollectionView от пользователей на страницу создания группы Create_group

---
+++ SmartStudy/Models/User_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartStudy.ModelsDB;
+using System.Collections.ObjectModel;
+
+namespace SmartStudy.Models
+{
+    class User_list
+    {
+        public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
+
+        public User_list() => Load_All_Users();
+
+        public async void Load_All_Users()
+        {
+            List<User> users = await Client.GetUsersList();
+            //Users.Clear();
+            foreach (User user in users)
+                Users.Add(user);
+        }
+    }
+}
