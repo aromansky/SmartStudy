cebc77dd2e521f64d4c4f15184b6e64166d0a874
aromansky <kirbi.zsthan@gmail.com>
2024-02-28 23:45:03 +0300

    Контроллер для homework
    Реализованы методы:
    - Добавление дз
    - Редактирование дз
    - Удаление дз

--- WebAPI/Controllers/homeworkController.cs
+++ WebAPI/Controllers/homeworkController.cs
@@ -34,37 +34,17 @@ namespace WebAPI.Controllers
             return homework;
         }
 
-
-
-        // GET: api/event/homework_user-5
-        [HttpGet("homework_user-{id}")]
-        public async Task<ActionResult<IEnumerable<homework>>> GetHomeWorksWithUsers(long id)
-        {
-            var homeworks = from homework in _context.homework
-                            join user_homework in _context.user_homework on homework.homework_id equals user_homework.homework_id
-                            join user in _context.user on user_homework.user_id equals user.user_id
-                            select new homework
-                            {
-                                homework_id = homework.homework_id,
-                                Title = homework.Title,
-                                Description = homework.Description
-                            };
-            if (homeworks == null)
-                return NotFound();
-            return await homeworks.ToListAsync();
-        }
-
-        // PUT: api/event/5
+        // PUT: api/homework/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutEvent(long id, @event @event)
+        public async Task<IActionResult> PutHomework(long id, homework homework)
         {
-            if (id != @event.event_id)
+            if (id != homework.homework_id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(@event).State = EntityState.Modified;
+            _context.Entry(homework).State = EntityState.Modified;
 
             try
             {
@@ -72,7 +52,7 @@ namespace WebAPI.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!EventExists(id))
+                if (!HomeWorkExists(id))
                 {
                     return NotFound();
                 }
@@ -85,44 +65,46 @@ namespace WebAPI.Controllers
             return NoContent();
         }
 
-        // POST: api/Event
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<@event>> PostEvent(@event @event)
-        {
-          if (_context.@event == null)
-          {
-              return Problem("Entity set 'SmartStudyContext.event'  is null.");
-          }
-            _context.@event.Add(@event);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetEvent", new { id = @event.event_id }, @event);
-        }
-
-        // DELETE: api/event/5
+        // DELETE: api/homework/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEvent(long id)
+        public async Task<IActionResult> DeleteHomework(long id)
         {
-            if (_context.@event == null)
+            if (_context.homework == null)
             {
                 return NotFound();
             }
-            var @event = await _context.@event.FindAsync(id);
-            if (@event == null)
+            var homework = await _context.homework.FindAsync(id);
+            if (homework == null)
             {
                 return NotFound();
             }
 
-            _context.@event.Remove(@event);
+            _context.homework.Remove(homework);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-        private bool EventExists(long id)
+
+        // POST: api/homework
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<homework>> PostHomework(homework homework)
         {
-            return (_context.@event?.Any(e => e.event_id == id)).GetValueOrDefault();
+            if (_context.homework == null)
+            {
+                return Problem("Entity set 'SmartStudyContext.homework'  is null.");
+            }
+            _context.homework.Add(homework);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Gethomework", new { id = homework.homework_id }, homework);
+        }
+
+        private bool HomeWorkExists(long id)
+        {
+            return (_context.homework?.Any(e => e.homework_id == id)).GetValueOrDefault();
         }
     }
 }
