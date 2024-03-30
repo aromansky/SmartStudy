bea4ba832f2c12a3162c48641a0225e25b85ce81
aromansky <kirbi.zsthan@gmail.com>
2024-03-30 23:20:53 +0300

    Добавил поля со временем для дз

--- WebAPI/Models/homework.cs
+++ WebAPI/Models/homework.cs
@@ -10,5 +10,7 @@ namespace WebAPI.Models
         public required string Title { get; set; }
         public string? Description { get; set; }
         public long? author_id { get; set; }
+        public required DateTime date_begin { get; set; }
+        public required DateTime date_end { get; set; }
     }
 }
